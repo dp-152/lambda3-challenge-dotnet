23bfb1648f4ac75e64459ec8f7e91975854e133e
Mizael Pinto Almeida <mzalmeida.a@gmail.com>
2022-10-26 22:05:49 -0300

    feat: added api versioning

--- CopaGames.API/Program.cs
+++ CopaGames.API/Program.cs
@@ -1,8 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddApiVersioning(options =>
+{
+    options.ReportApiVersions = true;
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.DefaultApiVersion = new ApiVersion(1, 0);
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
