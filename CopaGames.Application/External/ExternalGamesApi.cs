863304f106d0c2f00fa28ee501852a3f06fd6b9f
Mizael Pinto Almeida <mzalmeida.a@gmail.com>
2022-10-26 23:46:17 -0300

    feat: implemented service for fetching data from API

---
+++ CopaGames.Application/External/ExternalGamesApi.cs
@@ -0,0 +1,36 @@
+using CopaGames.Application.External.Interfaces;
+using CopaGames.Domain.DTO.External.GamesApi;
+using Newtonsoft.Json;
+
+namespace CopaGames.Application.External;
+
+public class ExternalGamesApi : IGamesApi
+{
+    private readonly IHttpClientFactory _clientFactory;
+
+    public ExternalGamesApi(IHttpClientFactory clientFactory)
+    {
+        _clientFactory = clientFactory ?? throw new ArgumentNullException(nameof(clientFactory));
+    }
+    
+    public async Task<IEnumerable<GamesApiResponseDTO>> GetGameList()
+    {
+        HttpRequestMessage httpRequest = new(HttpMethod.Get, new Uri("/Competidores?copa=games"));
+        using var client = _clientFactory.CreateClient("CopaGamesApi");
+        var response = await client.SendAsync(httpRequest);
+        var content = await response.Content.ReadAsStringAsync();
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new HttpRequestException($"Error sending request: {response.StatusCode}: {response.ReasonPhrase} - {content}");
+        }
+
+        var result = JsonConvert.DeserializeObject<IEnumerable<GamesApiResponseDTO>>(content);
+
+        if (result is null)
+        {
+            throw new Exception("Empty response from API");
+        }
+
+        return result;
+    }
+}
