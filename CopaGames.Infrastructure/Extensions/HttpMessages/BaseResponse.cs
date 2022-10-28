e8fa691bbfc32c8f38bbb7df5cc9b59d89b28ef3
Mizael Pinto Almeida <mzalmeida.a@gmail.com>
2022-10-28 00:06:18 -0300

    feat: updated BaseResponse fields

--- CopaGames.Infrastructure/Extensions/HttpMessages/BaseResponse.cs
+++ CopaGames.Infrastructure/Extensions/HttpMessages/BaseResponse.cs
@@ -6,10 +6,11 @@ namespace CopaGames.Infrastructure.Extensions.HttpMessages;
 
 public class BaseResponse
 {
-    public string RequestId { get; set; }
-    public string Message { get; set; }
-    public string ErrorDetails { get; set; }
-    public IEnumerable<string> ErrorList { get; set; }
+    public string RequestId { get; set; } = "";
+    public string Message { get; set; } = "";
+    public string? ErrorDetails { get; set; }
+    public string? ErrorStack { get; set; }
+    public IEnumerable<string>? ErrorList { get; set; }
     
     [JsonIgnore, NotMapped]
     public EResultType ResultType { get; set; }
