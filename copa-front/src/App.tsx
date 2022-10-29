ac7e707e4dbf8b8835cb99086fd83c870ed6c195
Mizael Pinto Almeida <mzalmeida.a@gmail.com>
2022-10-29 14:32:07 -0300

    feat: added main routes

--- copa-front/src/App.tsx
+++ copa-front/src/App.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
+import GameSelect from './pages/GameSelect';
+import Home from './pages/Home';
+import TournamentResult from './pages/TournamentResult';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/select-games" element={<GameSelect />} />
+        <Route path="/tournament-result" element={<TournamentResult />} />
+        <Route path='*' element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ copa-front/src/pages/GameSelect.tsx
@@ -0,0 +1,3 @@
+export default function GameSelect() {
+  return <div>GameSelect</div>;
+}
