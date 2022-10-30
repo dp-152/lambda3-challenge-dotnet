cb2a3973a75af31c6960bb22fdd84f6333ad481a
Mizael Pinto Almeida <mzalmeida.a@gmail.com>
2022-10-30 11:58:27 -0300

    feat: added repository for API calls

---
+++ copa-front/src/repository/gameApiRepository.ts
@@ -0,0 +1,29 @@
+import IAPIResponse from "../interfaces/IAPIResponse";
+import IGameData from "../interfaces/IGameData";
+import axios from "axios";
+import ITournamentRequest from "../interfaces/ITournamentRequest";
+import ITournamentResponse from "../interfaces/ITournamentResponse";
+
+const axiosInstance = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+});
+
+export async function getAllGames(): Promise<IGameData> {
+  const { data: responseData } = await axiosInstance
+    .get<IAPIResponse<IGameData>>("/game");
+
+  return responseData.data;
+}
+
+export async function runTournament(contestants: ITournamentRequest) {
+  if (contestants.gameIds.length !== 8) {
+    throw new Error(
+      `Must send exactly 8 contestants, sent ${contestants.gameIds.length}`,
+    );
+  }
+
+  const { data: responseData } = await axiosInstance
+    .post<IAPIResponse<ITournamentResponse>>("/game");
+
+  return responseData;
+}
