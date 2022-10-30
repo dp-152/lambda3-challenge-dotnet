3b294a99cc130dd225153ff68dbf34d59cf982a9
Mizael Pinto Almeida <mzalmeida.a@gmail.com>
2022-10-30 16:46:16 -0300

    feat: using selected counter component

--- copa-front/src/pages/GameSelect.tsx
+++ copa-front/src/pages/GameSelect.tsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
 import Container from "react-bootstrap/Container";
 import TitleCard from "../components/page/GameSelect/TitleCard";
+import SelectedCounter from "../components/page/GameSelect/SelectedCounter";
 
 export default function GameSelect() {
+  const [selected, setSelected] = useState(0);
+
   return (
-    <Container as="header">
-      <TitleCard />
+    <Container>
+      <Container as="header">
+        <TitleCard />
+      </Container>
+      <Container as="nav">
+        <SelectedCounter selected={selected} />
+      </Container>
     </Container>
   );
 }
