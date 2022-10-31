b38aa1555cd98a150d90fbc7f441fd6df95e7dd0
Mizael Pinto Almeida <mzalmeida.a@gmail.com>
2022-10-31 16:22:07 -0300

    refactor: removed redundant home page

--- copa-front/src/App.tsx
+++ copa-front/src/App.tsx
@@ -1,15 +1,13 @@
 import "./assets/css/bootstrap.css";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import GameSelect from "./pages/GameSelect";
-import Home from "./pages/Home";
 import TournamentResult from "./pages/TournamentResult";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/select-games" element={<GameSelect />} />
+        <Route path="/" element={<GameSelect />} />
         <Route path="/tournament-result" element={<TournamentResult />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
